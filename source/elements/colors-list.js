194abc1538ecbd0798a4794ecaccf8233d335c62
Gautam Chekuri <gautam.chekuri@gmail.com>
2014-05-05 14:38:35 -0400

    1) Adding classes that are used to provide margins for elements 2) Adding classes for color button

---
+++ source/elements/colors-list.js
@@ -0,0 +1,10 @@
+Polymer(
+    "colors-list", {
+        colors: [],
+        ready: function(){
+            this.colors = [
+                { name: "red" }, { name: "green" }, { name: "blue" }, { name: "white" }
+            ]
+        }
+    }
+);
