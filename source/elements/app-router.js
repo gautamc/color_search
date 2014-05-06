86a4e5a09345d0a5043fb05e1a7e52c0b45a5e39
Gautam Chekuri <gautam.chekuri@gmail.com>
2014-05-06 07:42:04 -0400

    improving the router implementation

--- source/elements/app-router.js
+++ source/elements/app-router.js
@@ -6,7 +6,6 @@ Polymer(
 
             console.log( this.get_route() );
 
-            // Observe window.location.hash for changes
             var observer = new PathObserver(window, 'location.hash');
             observer.open(function(new_value, old_value) { 
                 my.handle_route_change(new_value, old_value);
@@ -14,11 +13,23 @@ Polymer(
         },
         get_route: function() {
             var path = window.location.hash || "#";
-            var matching_route = this.routes.filter(function(route){ return route.urn == path; })[0];
+            var matching_route = this.routes.filter(function(route){
+                if( route.urn.match(/:\w+/) != null ) {
+                    var urn_re = new RegExp( route.urn.replace(/:\w+/, '.+') );
+                    if ( path.match(urn_re) != null ) {
+                        return true;
+                    }
+                } else {
+                    return route.urn == path;
+                }
+            })[0];
             return matching_route;
         },
         handle_route_change: function(new_route, old_route) {
             console.log( this.get_route() );
+        },
+        render_view: function(){
+            
         }
     }
 );
