e27f77367279bcafd62d1fd8b99c923fd358691e
Gautam Chekuri <gautam.chekuri@gmail.com>
2014-05-06 10:13:48 -0400

    displaying input parameter in the results section

--- source/elements/home-view.js
+++ source/elements/home-view.js
@@ -1,11 +1,12 @@
 Polymer(
     "home-view", {
         params: {},
+        results: "",
         ready: function(){
-            console.log( this.params );
+            //console.log( this.params );
         },
         paramsChanged: function(old_value, new_value) {
-            console.log( this.params );
+            this.results = "Query and Display results for color: " + new_value.input;
         }
     }
 );
