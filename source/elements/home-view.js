391bc1c64ec40e540f271e8eb9ad6f85a9a36268
Gautam Chekuri <gautam.chekuri@gmail.com>
2014-05-06 19:36:54 -0400

    1) spacing out the functions to make the easier to read 2) Initializing spectrum color-picker in the ready block 3) Making sure undefined color value is not displayed in the `results` string

--- source/elements/home-view.js
+++ source/elements/home-view.js
@@ -1,14 +1,29 @@
 Polymer(
+
     "home-view", {
+
         ready: function(){
             this.params = {};
             this.results = "";
+
+            jQuery(this.$.color_picker).spectrum({
+                preferredFormat: "hex",
+                flat: true,
+                showInput: true,
+                allowEmpty:true
+            });
         },
+
         paramsChanged: function(old_value, new_value) {
             this.render();
         },
+
         render: function(){
-            this.results = "Query and Display results for color: " + this.params.input;
+            this.results = this.params.input ?
+                "Query and Display results for color: " + this.params.input :
+                "No color selected";
         }
+
     }
+
 );
