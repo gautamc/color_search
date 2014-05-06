1fbf1838626931548b6a56041e8b22c65c265ab0
Gautam Chekuri <gautam.chekuri@gmail.com>
2014-05-06 10:07:13 -0400

    initializing jquery in polymer-ready

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,16 +1,36 @@
 $.noConflict();
 
 jQuery(document).ready(function(){
-    jQuery("#color-picker").spectrum({
-        preferredFormat: "hex",
-        flat: true,
-        showInput: true,
-        allowEmpty:true
-    });
+    /*
+    window.setTimeout(
+        function() {
+            jQuery("#color-picker-box").append('<input type="text" id="color-picker" />')
+            console.log( jQuery("#color-picker") );
+        }, 2000
+    );
+    */
 });
 
 document.addEventListener(
     "DOMContentLoaded",
     function(){
+    }
+)
+
+document.addEventListener(
+    "WebComponentsReady",
+    function(){
+    }
+)
+
+document.addEventListener(
+    "polymer-ready",
+    function(){
+        jQuery("#color-picker").spectrum({
+            preferredFormat: "hex",
+            flat: true,
+            showInput: true,
+            allowEmpty:true
+        });
     }
 )
