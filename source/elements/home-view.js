c61e4b2c124efe5a7682e18e4d7409c1bdbd1853
Gautam Chekuri <gautam.chekuri@gmail.com>
2014-05-07 12:42:33 -0400

    1) Adding polymer-ajax element 2) Using polymer-ajax element to query a server and render results.

--- source/elements/home-view.js
+++ source/elements/home-view.js
@@ -22,6 +22,16 @@ Polymer(
             this.results = this.params.input ?
                 "Query and Display results for color: " + this.params.input :
                 "No color selected";
+            var ajax = this.$.google_feed;
+            ajax.params = {"alt":"json", "q":"firefox"};
+            ajax.go();
+        },
+
+        feed_results_callback: function(e) {
+            var listing_element = this.$.results_listing;
+            listing_element.model = {
+                response: e.detail.response
+            }
         }
 
     }
