18cda57842a838ca0c1e176e0814d03a3cb85e0e
Gautam Chekuri <gautam.chekuri@gmail.com>
2014-05-08 15:05:01 -0400

    Automated commit at 2014-05-08 19:05:00 UTC by middleman-deploy 0.2.3

--- elements/home-view.js
+++ elements/home-view.js
@@ -4,7 +4,7 @@ Polymer(
 
         ready: function(){
             this.params = {};
-            this.results = "";
+            this.loading = false;
 
             jQuery(this.$.color_picker).spectrum({
                 preferredFormat: "hex",
@@ -22,19 +22,21 @@ Polymer(
         },
 
         render: function(){
-            this.results = this.params.input ?
-                "Query and Display results for color: " + this.params.input :
-                "No color selected";
+            this.loading = true;
             var ajax = this.$.rately_feed;
             ajax.body = JSON.stringify( { payload: { colors: [this.params.input == null ? "" : this.params.input], keywords: ["candle"] } } );
             ajax.go();
         },
 
-        feed_results_callback: function(e) {
+        feed_results_callback: function(response, xhr) {
             var listing_element = this.$.results_listing;
             listing_element.model = {
-                response: e.detail.response
+                response: response.detail.response
             }
+        },
+
+        feed_results_complete_callback: function(response, xhr) {
+            this.loading = false;
         }
 
     }
